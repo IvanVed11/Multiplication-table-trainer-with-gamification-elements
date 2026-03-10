44afd43415a92eef854ecddc84bab610248fb4e1
ivan vedernikov <ivan.ved2020@mail.ru>
2026-03-10 20:40:52 +0300

    изменено название раздела топ-5 легенд умножения на зал славы

--- keyboards/menu_commands.py
+++ keyboards/menu_commands.py
@@ -5,7 +5,7 @@ async def set_main_menu(bot: Bot):
     menu_commands = [
         BotCommand(command = "start", description="🎓 Тренировка"),
         BotCommand(command = "profile", description="📊 Мои достижения"),
-        BotCommand(command = "top", description="🏆 Топ 5 легенд умножения"),
+        BotCommand(command = "top", description="🏆 Зал славы"),
         BotCommand(command = "help", description="🆘 Помощь"),
     ]
     await bot.set_my_commands(commands=menu_commands, scope=BotCommandScopeAllPrivateChats())
